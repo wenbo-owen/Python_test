2ea3d3b047263502045cf7a0bd7efb2336f081c2
wenbo-owen <313864336@qq.com>
2024-07-13 21:30:27 +0800

    Changes to be committed: 	modified:   chapter8/8-16 Test1.py

--- chapter8/8-16 Test1.py
+++ chapter8/8-16 Test1.py
@@ -1,2 +1,9 @@
 lst = [40,76,28,25,42,17,56,53,43,20,76]
-print('使用内置函数得到的ma是',max(lst))
+print('使用内置函数得到的最大值是',max(lst))
+
+res = lst[0]
+for i in range(len(lst)):
+    if lst[i] >= res :
+        res = lst[i]
+
+print('使用非max函数得到的结果是：',res)
