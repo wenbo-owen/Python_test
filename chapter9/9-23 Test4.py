a3713d395e0f5969aa175677833224bfb5a68cb6
wenbo-owen <313864336@qq.com>
2024-07-16 15:06:41 +0800

    Changes to be committed: 	modified:   .idea/misc.xml 	modified:   chapter9/9-23 Test4.py

--- chapter9/9-23 Test4.py
+++ chapter9/9-23 Test4.py
@@ -23,3 +23,25 @@ class Taxi(Car):
     def stop(self):
         print('目的地到了，请您扫码付款，欢迎下次再来')
 
+
+class FamilyCar(Car):
+    def __init__(self,type, no, name):
+        super().__init__(type, no)
+        self.name = name
+
+    #重写父类的方法
+    def start(self):
+        print(f'我是{self.name}，我的轿车我做主')
+
+    def stop(self):
+        print(f'目的地到了，我们去玩吧')
+
+taix = Taxi('上海大众','京A88888','长城')
+taix.start()
+taix.stop()
+
+print('-'*50)
+
+family = FamilyCar('奔驰','黑A99999','温博')
+family.start()
+family.stop()
